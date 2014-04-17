81d78ae4ca3cc3dce91041709e152c4db2aac876
Nikolay Shopik <shopik@inblock.ru>
2014-04-17 18:57:02 +0400

    Time handling

--- dummy-parser.py
+++ dummy-parser.py
@@ -3,8 +3,9 @@ import re
 f = open("iridum.mail")
 lines = f.read()
 LON=lines.splitlines()
+TIME=LON[-1].split(" ")
 POSITION=re.split('=|&',LON[-1])
 LAT=(POSITION[-3])
 LON=(POSITION[-1])
-print(LAT,LON)
+print(LAT,LON,TIME[-4:-1])
 f.close

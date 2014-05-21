dd720d1ef99f7079c2c227f2074e2961fda5245d
Nikolay Shopik <shopik@inblock.ru>
2014-05-21 14:12:36 +0400

    altitude parsing and exporting

--- dummy-parser.py
+++ dummy-parser.py
@@ -11,8 +11,10 @@ URL = []
 TXT = []
 LAT = []
 LON = []
+ALT = []
 TIME = []
 for index in range(len(MSG)):
+	ALT.append(MSG[index][3])
 	UTC.append(MSG[index][7:10])
 	URL.append(re.split('=|&', MSG[index][10]))	
 	TXT.append(MSG[index][11:])
@@ -23,7 +25,7 @@ for index in range(len(UTC)):
 	TIME.append(UTC[index][0] + " " + UTC[index][1] + " " + UTC[index][2])
 data = []
 for index in range(len(MSG)):
-	data.append({'lat':LAT[index], 'lng':LON[index], 'title':TIME[index], 'content':' '.join(TXT[index])})
+	data.append({'alt':ALT[index], 'lat':LAT[index], 'lng':LON[index], 'title':TIME[index], 'content':' '.join(TXT[index])})
 
 j = json.dumps(data, indent=2)
 f = open('map.json', 'w')

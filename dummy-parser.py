c5735f45d454e1cd9b7263db849ad1ed67a92d92
Nikolay Shopik <shopik@inblock.ru>
2014-05-22 17:15:23 +0400

    Convert date and time to ISO 8601 format

--- dummy-parser.py
+++ dummy-parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import re
 import json
+from datetime import datetime as dt
 RAWDATA = []
 for line in open("../draga_mail"):
 	if "http://" in line:
@@ -13,19 +14,23 @@ LAT = []
 LON = []
 ALT = []
 TIME = []
+ISO = []
+time_parsed = []
 for index in range(len(MSG)):
 	ALT.append(MSG[index][3])
-	UTC.append(MSG[index][7:10])
+	UTC.append(MSG[index][7:9])
 	URL.append(re.split('=|&', MSG[index][10]))	
 	TXT.append(MSG[index][11:])
 for index in range(len(URL)):
 	LAT.append(URL[index][1])
 	LON.append(URL[index][3])
 for index in range(len(UTC)):
-	TIME.append(UTC[index][0] + " " + UTC[index][1] + " " + UTC[index][2])
+	TIME.append(UTC[index][0] + " " + UTC[index][1])
+	time_parsed.append(dt.strptime(TIME[index], "%d-%b-%Y %H:%M:%S"))
+	ISO.append(dt.strftime(time_parsed[index], "%Y-%m-%d %H:%M:%SZ"))
 data = []
-for index in range(len(MSG)):
-	data.append({'alt':ALT[index], 'lat':LAT[index], 'lng':LON[index], 'title':TIME[index], 'content':' '.join(TXT[index])})
+for i in range(len(MSG)):
+	data.append({'alt':ALT[i], 'lat':LAT[i], 'lng':LON[i], 'title':ISO[i], 'content':' '.join(TXT[i])})
 
 j = json.dumps(data, indent=2)
 f = open('map.json', 'w')
